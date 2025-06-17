41841ba0df9ce7564ff09ae68f769efc9d2dc65d
T-Yuto <k23082kk@aitech.ac.jp>
2025-06-17 09:48:31 +0900

    最初のコミット
    テスト

---
+++ sample.py
@@ -0,0 +1,9 @@
+# sample.py
+
+import sys  # sysモジュールをインポートして、コマンドライン引数を扱う
+
+# 入力されたメッセージを取得
+message = sys.argv[1]
+
+# 取得したメッセージを表示
+print(message)
---
+++ sample2.py
@@ -0,0 +1,7 @@
+# sample2.py
+
+# 入力されたメッセージを取得
+message = 'テスト'
+
+# 取得したメッセージを表示
+print(message)
